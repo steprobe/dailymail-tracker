7b2675b6f45eeb0927c665a1e7018af2b0408915
Stephen Roberts <stephen.roberts4@gmail.com>
2021-03-22 15:47:32 +0100

    Github actions + deploy to heroku

--- jobs/track.js
+++ jobs/track.js
@@ -1,20 +1,14 @@
 const cheerio = require('cheerio');
 const got = require('got');
 
-const matches = (i, link) => {
-  return link.attribs.href !== undefined && link.attribs.href.includes('ample');
-};
-
 const track = async () => {
   const response = await got('https://www.dailymail.co.uk/home/index.html');
   const $ = cheerio.load(response.body);
 
-  $('a')
-    .filter(matches)
-    .each((i, link) => {
-      const { href, title } = link.attribs;
-      console.log(link);
-    });
+  $('a').each((i, link) => {
+    // const { href, title } = link.attribs;
+    console.log(link);
+  });
 };
 
 module.exports = track;

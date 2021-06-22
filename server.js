218a3382a321f79276197eecfa1b66d5c7a1c525
Stephen Roberts <stephen.roberts@instaply.com>
2021-06-22 11:40:20 +0200

    Adding cronjob for daily sync

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 require('dotenv').config();
 const express = require('express');
+const { CronJob } = require('cron');
 const listEndpoints = require('express-list-endpoints');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const Sentry = require('@sentry/node');
 const connectToDb = require('./config/db');
+const track = require('./jobs/track');
 
-Sentry.init({
-  environment: process.env.NODE_ENV,
-  dsn: process.env.SENTRY_DNS,
-  tracesSampleRate: 1.0,
-});
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    environment: process.env.NODE_ENV,
+    dsn: process.env.SENTRY_DNS,
+    tracesSampleRate: 1.0,
+  });
+}
 
 const app = express();
 
@@ -35,3 +39,19 @@ console.log(listEndpoints(app));
 
 const port = process.env.NODE_ENV === 'test' ? 5001 : process.env.PORT || 5000;
 app.listen(port, () => console.log(`Running on ${port}`));
+
+const everyMorning = '0 10 * * *';
+const morningKeywordsSync = new CronJob(
+  everyMorning,
+  async () => {
+    console.log(
+      `Morning cron job to sync keywords is live :D ${new Date().toISOString()}`,
+    );
+    await track();
+  },
+  null,
+  false,
+  'Europe/Madrid',
+);
+
+morningKeywordsSync.start();
